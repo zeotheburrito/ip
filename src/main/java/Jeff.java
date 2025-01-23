06fd91a9a5ad7ac664f3550a4b679c70cfc402f7
zeotheburrito <wongzenwei225@gmail.com>
2025-01-23 23:18:05 +0800

    Finished Level 3. Mark as Done

--- src/main/java/Jeff.java
+++ src/main/java/Jeff.java
@@ -3,6 +3,30 @@ import java.util.Scanner;
 import java.util.ArrayList;
 
 public class Jeff {
+    public static class Task {
+        private String desc;
+        private boolean done;
+        public Task(String desc) {
+            this.desc = desc;
+            this.done = false;
+        }
+
+        public void setDone() {
+            this.done = true;
+        }
+
+        public void setNotDone() {
+            this.done = false;
+        }
+
+        @Override
+        public String toString() {
+            if (this.done) {
+                return "[X] " + this.desc;
+            }
+            return "[ ] " + this.desc;
+        }
+    }
     public static void main(String args[]) {
         String greetings = "____________________________________________________________\n" +
                 " Hello! I'm Jeff\n" +
@@ -10,32 +34,51 @@ public class Jeff {
                 "____________________________________________________________\n";
 
         String exitMsg = "Bye. Hope to see you again soon!";
-        String exitPhrase = "bye";
-        String listPhrase = "list";
 
-        ArrayList<String> list = new ArrayList<String>();
+        ArrayList<Task> list = new ArrayList<Task>();
 
         System.out.println(greetings);
 
         Scanner sc = new Scanner(System.in);
         String input = sc.nextLine();
 
-        while (!input.equals(exitPhrase)) {
+        while (!input.equals("bye")) {
+            String[] parsed = input.split(" ");
             String message = "";
-            if (input.equals(listPhrase)) {
-                String items = "";
-                for (int i = 0; i < list.size(); i++) {
-                    items += " " + (i + 1) + ". " + list.get(i) + "\n";
-                }
-                message = "____________________________________________________________\n" +
-                        items +
-                        "____________________________________________________________\n";
-            } else {
-                list.add(input);
-                message = "____________________________________________________________\n" +
-                        " added: " + input + "\n" +
-                        "____________________________________________________________\n";
+            switch (parsed[0]) {
+                case "list":
+                    String items = "";
+                    for (int i = 0; i < list.size(); i++) {
+                        items += " " + (i + 1) + ". " + list.get(i) + "\n";
+                    }
+                    message = "____________________________________________________________\n" +
+                            " Here are the tasks in your list:\n" +
+                            items +
+                            "____________________________________________________________\n";
+                    break;
+                case "mark":
+                    int index = Integer.parseInt(parsed[1]) - 1;
+                    list.get(index).setDone();
+                    message = "____________________________________________________________\n" +
+                            " Nice! I've marked this task as done:\n" +
+                            "  " + list.get(index) + "\n" +
+                            "____________________________________________________________\n";
+                    break;
+                case "unmark":
+                    int ind = Integer.parseInt(parsed[1]) - 1;
+                    list.get(ind).setNotDone();
+                    message = "____________________________________________________________\n" +
+                            " OK, I've marked this task as not done yet:\n" +
+                            "  " + list.get(ind) + "\n" +
+                            "____________________________________________________________\n";
+                    break;
+                default:
+                    list.add(new Task(input));
+                    message = "____________________________________________________________\n" +
+                            " added: " + input + "\n" +
+                            "____________________________________________________________\n";
             }
+
             System.out.println(message);
             input = sc.nextLine();
         }
