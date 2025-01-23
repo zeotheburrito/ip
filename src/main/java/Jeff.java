b7d864f7bc57d593a599efebb1083e60e62b0714
zeotheburrito <wongzenwei225@gmail.com>
2025-01-23 16:49:13 +0800

    Finished Level 0. Renamed chatbot and added greetings.

---
+++ src/main/java/Jeff.java
@@ -0,0 +1,12 @@
+public class Jeff {
+    public static void main(String args[]) {
+        String display = "____________________________________________________________\n" +
+                " Hello! I'm Jeff\n" +
+                " What can I do for you?\n" +
+                "____________________________________________________________\n" +
+                " Bye. Hope to see you again soon!\n" +
+                "____________________________________________________________\n";
+
+        System.out.println(display);
+    }
+}
