f2a3655ff4d3dfbb6ddce9a0235d0a829ada118a
zeotheburrito <wongzenwei225@gmail.com>
2025-01-23 17:18:31 +0800

    Finished Level 1. Echo

--- src/main/java/Jeff.java
+++ src/main/java/Jeff.java
@@ -1,12 +1,32 @@
+import java.util.Objects;
+import java.util.Scanner;
+
 public class Jeff {
     public static void main(String args[]) {
-        String display = "____________________________________________________________\n" +
+        String greetings = "____________________________________________________________\n" +
                 " Hello! I'm Jeff\n" +
                 " What can I do for you?\n" +
-                "____________________________________________________________\n" +
-                " Bye. Hope to see you again soon!\n" +
                 "____________________________________________________________\n";
 
-        System.out.println(display);
+        String exitMsg = "Bye. Hope to see you again soon!";
+        String exitPhrase = "bye";
+
+        System.out.println(greetings);
+
+        Scanner sc = new Scanner(System.in);
+        String input = sc.nextLine();
+
+        do {
+            String message = "____________________________________________________________\n" +
+                        " " + input + "\n" +
+                        "____________________________________________________________\n";
+            System.out.println(message);
+            input = sc.nextLine();
+        } while (!input.equals(exitPhrase));
+
+        String message = "____________________________________________________________\n" +
+                " " + exitMsg + "\n" +
+                "____________________________________________________________\n";
+        System.out.println(message);
     }
 }
